4910a5c84be9a56e9c16f0f47dce5b822c361db4
karenmayfelipe <karenmay.felipe@carsu.edu.ph>
2023-01-31 18:53:23 +0800

    save project's changes

---
+++ resources/views/users/users.blade.php
@@ -0,0 +1,50 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Users') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+
+                    <h2 class="float-left">
+                        {{$header}}
+                    </h2>
+
+                    <button class="float-right rounded-full bg-orange-400 p-1 hover:bg-orange-500">
+                        Add Users
+                    </button>
+
+
+                    <table class="table-auto w-full">
+                        <thead>
+                            <tr>
+                               <th>Name</th>
+                               <th>Email</th>
+                               <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        @foreach ($users as $user)
+                            <tr>
+                               <td>{{$user->name}}</td>
+                               <td class="text-center">{{$user->email}}</td>
+                               <td class="text-center">
+                                    <button class="rounded-full bg-orange-700 p-1 hover:bg-orange-500">Update</button>
+                                    <button class="rounded-full bg-orange-700 p-1 hover:bg-orange-500">Delete</button>
+                               </td>
+                            </tr>
+                        @endforeach
+                            
+                        </tbody>
+                      </table>
+                                       
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
